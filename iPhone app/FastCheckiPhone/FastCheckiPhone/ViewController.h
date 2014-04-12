487b9355c40aaa0b202cd41de2d1564103b26954
Canzhi Ye <canzhiye@gmail.com>
2014-04-11 23:44:08 -0700

    created iPhone and iPad projects
    the first of just a few actual commit logs

---
+++ iPhone app/FastCheckiPhone/FastCheckiPhone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FastCheckiPhone
+//
+//  Created by Canzhi Ye on 4/11/14.
+//  Copyright (c) 2014 we so cool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iPhone app/FastCheckiPhone/FastCheckiPhone/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FastCheckiPhone
+//
+//  Created by Canzhi Ye on 4/11/14.
+//  Copyright (c) 2014 we so cool. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
